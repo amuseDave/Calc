20b1f47c311613f4fb68b2e08dc34696e691a9cc
amuseDave <amusedave2@gmail.com>
2025-04-22 18:03:37 +0300

    add addition and substraction functionality

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ const buttonsGrid = document.getElementById("buttons-grid");
 const monitorTextContainerEl = document.getElementById("monitor-text-container");
 const monitorTextEl = document.getElementById("monitor-text");
 const monitorLetterEl = document.getElementById("monitor-letter-placeholder");
+const secondMonitorEl = document.getElementById("second-monitor");
 
 const monitorValues = ["0"];
+const calculatorState = {
+  isDot: false,
+  isPlusMinus: false,
+  curVal: 0,
+  curOperation: "",
+};
+
 monitorTextEl.textContent = monitorValues[0];
+// secondMonitorEl.textContent = monitorValues[0];
 
 function handleCalculator(e) {
   const button = e.target.closest(".button");
@@ -13,9 +22,16 @@ function handleCalculator(e) {
   const { value } = button.dataset;
   if (!value) return;
 
-  if (+value >= 0 && +value <= 9) {
-    handleNumbers(value);
-  }
+  if (+value >= 0 && +value <= 9) handleNumbers(value);
+  else if (value === "delete") handleDelete();
+  else if (value === "dot") handleDot();
+  else if (value === "clear") handleClear();
+  else if (value === "plus-or-minus") handlePlusMinus();
+  else if (value === "plus") handlePlus();
+  else if (value === "minus") handleMinus();
+
+  displaySecondMonitor();
+  displayMonitorValues();
 }
 
 function handleNumbers(value) {
@@ -34,8 +50,104 @@ function handleNumbers(value) {
     }
     monitorValues.push(value);
   }
+}
 
-  monitorTextEl.textContent = monitorValues.join("");
+function handleDelete() {
+  if (monitorValues.length > 1) {
+    const val = monitorValues.pop();
+    if (val === ".") calculatorState.isDot = false;
+  } else if (monitorValues[0] !== "0") {
+    resetMonitorValues();
+  }
+}
+function handleClear() {
+  if (monitorValues.length === 1 && monitorValues[0] === "0" && !calculatorState.curVal) {
+    return;
+  }
+  calculatorState.curVal = 0;
+  calculatorState.curOperation = "";
+  resetMonitorValues();
+}
+
+function handleDot() {
+  if (calculatorState.isDot) return;
+  else {
+    calculatorState.isDot = true;
+    monitorValues.push(".");
+  }
+}
+
+function handlePlusMinus() {
+  if (!isValidMonitor()) return;
+  calculatorState.isPlusMinus = !calculatorState.isPlusMinus;
+}
+
+function handlePlus() {
+  if (!isValidMonitor()) {
+    if (calculatorState.curVal) calculatorState.curOperation = " +";
+    return;
+  }
+
+  calculatorState.curOperation = " +";
+
+  // Convert string integer
+  // Add values for the calc
+  let val = +monitorValues.join("");
+  if (calculatorState.isPlusMinus) val = -val;
+
+  if (calculatorState.curVal) {
+    calculatorState.curVal = calculatorState.curVal + val;
+  } else {
+    calculatorState.curVal = val;
+  }
+}
+function handleMinus() {
+  if (!isValidMonitor()) {
+    if (calculatorState.curVal) calculatorState.curOperation = " -";
+    return;
+  }
+
+  calculatorState.curOperation = " -";
+
+  // Convert string integer
+  // Add values for the calc
+  let val = +monitorValues.join("");
+  if (calculatorState.isPlusMinus) val = -val;
+
+  if (calculatorState.curVal) {
+    calculatorState.curVal = calculatorState.curVal - val;
+  } else {
+    calculatorState.curVal = val;
+  }
+}
+
+function displayMonitorValues() {
+  const plusMinus = calculatorState.isPlusMinus ? "-" : "";
+  monitorTextEl.textContent = plusMinus + monitorValues.join("");
+}
+
+function displaySecondMonitor() {
+  const val = calculatorState.curVal;
+  const str = val + calculatorState.curOperation;
+  if (str === secondMonitorEl.textContent) return;
+
+  resetMonitorValues();
+  secondMonitorEl.textContent = str;
+}
+
+function resetMonitorValues() {
+  if (monitorValues.length === 1 && monitorValues[0] === "0") return;
+  monitorValues.splice(0, monitorValues.length);
+  monitorValues[0] = "0";
+  calculatorState.isPlusMinus = false;
+  calculatorState.isDot = false;
+}
+
+function isValidMonitor() {
+  if (!+monitorValues.join("")) {
+    resetMonitorValues();
+    return false;
+  } else return true;
 }
 
 buttonsGrid.addEventListener("click", handleCalculator);
